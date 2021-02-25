ea72ad64a503682be78770fafaff0ee88dfad42e
Noel1827 <noel.vargas5@upr.edu>
2021-02-25 01:08:48 -0400

    LinkedList base Class

---
+++ LinkedLists/LinkedList.java
@@ -0,0 +1,11 @@
+public class LinkedList {
+    private Node first;
+    private Node last;
+
+    // addFirst
+    // addLast
+    // deleteFirst
+    // deleteLast
+    // contains
+    // indexOf
+}
