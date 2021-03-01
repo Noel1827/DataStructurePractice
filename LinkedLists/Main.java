85644fcff2710278c9450bc63b8c805309452107
Noel1827 <noel.vargas5@upr.edu>
2021-03-01 01:21:47 -0400

    getKthFromTheEnd function

--- LinkedLists/Main.java
+++ LinkedLists/Main.java
@@ -1,7 +1,12 @@
 
 public class Main {
     public static void main(String args[]){
-  
+     LinkedList l = new LinkedList();
+        l.addLast(9);
+        l.addLast(2);
+        l.addLast(10);
+      System.out.println(l.getKthFromTheEnd(10));
+        // l.print();
     }
 }
  
