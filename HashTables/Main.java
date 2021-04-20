52a2cfca0f79fbfc5ad01dbd94ffb571edf85f63
Noel1827 <noel.vargas5@upr.edu>
2021-04-19 22:43:40 -0400

    refreshed memory on hashtables

--- HashTables/Main.java
+++ HashTables/Main.java
@@ -4,24 +4,41 @@ class Main {
 
     public static void main(String args[]) {
         // a green apple
-       System.out.println(findFirstNonRepeatingChar("a green apple")); 
+        System.out.println(findFirstNonRepeatingChar("a green apple"));
 
     }
 
-    public static char findFirstNonRepeatingChar(String str) {
+    // public static char findFirstNonRepeatingChar(String str) {
+    // Map<Character, Integer> map = new HashMap<>();
+    // var chars = str.toCharArray();
+    // for (char x : chars) {
+    // var count = (map.containsKey(x))? map.get(x):0;
+    // map.put(x,count+1);
+    // }
+
+    // for(char x: chars){
+    // if(map.get(x)==1)
+    // return x;
+    // }
+
+    // return 0;
+    // }
+
+    public static char findFirstNonRepeatingChar(String s) {
+
         Map<Character, Integer> map = new HashMap<>();
-        var chars = str.toCharArray();
-        for (char x : chars) {
-         var count = (map.containsKey(x))? map.get(x):0;
-         map.put(x,count+1);
+        char[] chars = s.toCharArray();
+
+        for (var x : chars) {
+           var count = (map.containsKey(x))? map.get(x):0;
+
+           map.put(x, count+1);
         }
 
-        for(char x: chars){
-            if(map.get(x)==1)
-            return x;
+        for (var x : chars) {
+            if (map.get(x) == 1)
+                return x;
         }
-       
-        return 0;
+        return Character.MIN_VALUE;
     }
-
 }
