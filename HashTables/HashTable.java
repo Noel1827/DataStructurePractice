cafe28e23afad602539d0f031aebdee65927f652
Noel1827 <noel.vargas5@upr.edu>
2021-04-30 16:18:29 -0400

    remove method

--- HashTables/HashTable.java
+++ HashTables/HashTable.java
@@ -13,34 +13,49 @@ public class HashTable {
 
     LinkedList<Entry>[] entries = new LinkedList[5];
 
-    public void put(int key,String value){
+    public void put(int key, String value) {
         int index = hash(key);
-        if(entries[index] == null)
-         entries[index] = new LinkedList<>();
+        if (entries[index] == null)
+            entries[index] = new LinkedList<>();
 
         var bucket = entries[index];
-        for(var entry: bucket){
-            if(entry.key == key){
+        for (var entry : bucket) {
+            if (entry.key == key) {
                 entry.value = value;
                 return;
             }
         }
-        bucket.addLast(new Entry(key,value));
+        bucket.addLast(new Entry(key, value));
     }
 
     private int hash(int key) {
         return key % entries.length;
     }
 
-    public String get(int key){
+    public String get(int key) {
         var bucket = entries[hash(key)];
 
-        if(bucket != null){
-        for(var entry:bucket){
-            if(key == entry.key)
-            return entry.value;
+        if (bucket != null) {
+            for (var entry : bucket) {
+                if (key == entry.key)
+                    return entry.value;
+            }
         }
-    }
         return null;
+    }
+
+    public void remove(int key) {
+        var bucket = entries[hash(key)];
+
+        if (bucket == null)
+            throw new IllegalStateException();
+
+        for (var entry : bucket) {
+            if (entry.key == key) {
+                bucket.remove(entry);
+                return;
+            }
+        }
+        throw new IllegalStateException();
     }
 }
--- HashTables/Main.java
+++ HashTables/Main.java
@@ -11,6 +11,7 @@ class Main {
        table.put(8,"B");
        table.put(11,"C");
        table.put(6,"A+");
+       table.remove(60);
       System.out.println(table.get(10));
     }
 
